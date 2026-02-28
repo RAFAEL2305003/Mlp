abb94ddc11676175ee42df96bcec520f94cec741
Rafael R. Oliveira <rafaelroliveira2003@gmail.com>
2026-02-28 18:58:39 -0300

    add loss file

---
+++ include/loss.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "matrix/matrix.h"
+
+double MSE_derivative(const Matrix<int>& Y, const Matrix<int>& y)
+{
+    double result = 0.0;
+    auto [rows, cols] = y.shape();
+    for(size_t i = 0; i < rows; i++)
+    {
+        result += (Y(i, 0) - y(i, 0));
+    }
+
+    return (-2.0/rows) * result;
+}
