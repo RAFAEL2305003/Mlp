d3f235561b32be6cc4e1d3503c63b238e9860c9c
Rafael R. Oliveira <rafaelroliveira2003@gmail.com>
2026-03-16 16:46:50 -0300

    add read csv lib

--- src/nn.cpp
+++ src/nn.cpp
@@ -1,6 +1,7 @@
 #include "../include/linear_layer.h"
 #include "../include/activation_layer.h"
 #include "../include/loss_layer.h"
+#include "../include/rapidcsv.h"
 
 class dense_layer
 {
@@ -39,15 +40,15 @@ class dense_layer
 			linear_layers.resize(num_layers);
 			activation_layers.resize(num_layers);
 
-			linear_layers[0] = nn::linear_layer<double>(2, 8, rng);
+			linear_layers[0] = nn::linear_layer<double>(4, 8, rng);
 			activation_layers[0] = nn::activation_layer<double>(activation::type::sigmoid);
 			for(size_t i = 1; i < num_layers - 1; i++)
 			{
 				linear_layers[i] = nn::linear_layer<double>(linear_layers[i - 1].output_size, 8, rng);
 				activation_layers[i] = nn::activation_layer<double>(activation::type::sigmoid);
 			}
-			linear_layers[num_layers - 1] = nn::linear_layer<double>(linear_layers[num_layers - 2].output_size, 1, rng);
-			activation_layers[num_layers - 1] = nn::activation_layer<double>(activation::type::sigmoid);
+			linear_layers[num_layers - 1] = nn::linear_layer<double>(linear_layers[num_layers - 2].output_size, 3, rng);
+			activation_layers[num_layers - 1] = nn::activation_layer<double>(activation::type::softmax);
 
 			mse_layer = nn::loss_layer<double>(loss::type::mse);
 		}
@@ -98,15 +99,35 @@ class dense_layer
 
 int main()
 {
-	math::matrix<double> x = math::matrix<double>(4, 2, {0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 1.0, 1.0});
-	math::matrix<double> y = math::matrix<double>(4, 1, {0.0, 1.0, 1.0, 0.0});
-
 	double lr = 0.1;
 	size_t epochs = 10'000;
-	size_t num_layers = 3;
+	size_t num_layers = 4;
+
+	rapidcsv::Document train_dataset("IrisTest.csv");
+
+	// creating the matrix of attributes
+	std::vector<double> sepal_l = train_dataset.GetColumn<double>("SepalLengthCm");
+	std::vector<double> sepal_w = train_dataset.GetColumn<double>("SepalWidthCm");
+	std::vector<double> petal_l = train_dataset.GetColumn<double>("PetalLengthCm");
+	std::vector<double> petal_w = train_dataset.GetColumn<double>("PetalWidthCm");
+	math::matrix<double> x(sepal_l.size(), 1, sepal_l);
+	x = concat(x, sepal_w);
+	x = concat(x, petal_l);
+	x = concat(x, petal_w);
+
+	// creating the matrix of labels
+	std::vector<double> y_setosa = train_dataset.GetColumn<double>("Species_Iris-setosa");
+	std::vector<double> y_versicolor = train_dataset.GetColumn<double>("Species_Iris-versicolor");
+	std::vector<double> y_virginica = train_dataset.GetColumn<double>("Species_Iris-virginica");
+	math::matrix<double> y(y_setosa.size(), 1, y_setosa); 
+	y = concat(y, y_versicolor);
+	y = concat(y, y_virginica);
+
 	dense_layer dense(epochs, lr, num_layers, x, y);
 	dense.train();	
-	dense.feedforward().print();	
+
+	math::matrix predictions = dense.feedforward();
+	predictions.print();
 
 	return 0;
 }
